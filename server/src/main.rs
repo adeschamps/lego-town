0eac34771a1d8a74e28be5e3b50085b3e2574060
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2016-10-24 23:33:20 -0400

    Move handle_connection to a function.

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,60 +3,64 @@ use std::str::from_utf8;
 use std::thread;
 use websocket::message::Type;
 use websocket::{Server, Message, Sender, Receiver};
+use websocket::server::{Connection};
+use websocket::stream::{WebSocketStream};
 
 fn main() {
     let server = Server::bind("0.0.0.0:1234").unwrap();
 
     for connection in server {
-        thread::spawn(move || {
-            let mut client =
-                connection.unwrap()
-                .read_request().unwrap()
-                .accept()
-                .send().unwrap();
+        thread::spawn(|| handle_connection(connection.unwrap()));
+    }
+}
 
-            let ip = client.get_mut_sender()
-                .get_mut()
-                .peer_addr()
-                .unwrap();
+fn handle_connection(connection: Connection<WebSocketStream, WebSocketStream>) {
+    let mut client =
+        connection
+        .read_request().unwrap()
+        .accept()
+        .send().unwrap();
 
-            println!("Connection from {}", ip);
+    let ip = client.get_mut_sender()
+        .get_mut()
+        .peer_addr()
+        .unwrap();
 
-            let (mut sender, mut receiver) = client.split();
+    println!("Connection from {}", ip);
 
-            for message in receiver.incoming_messages() {
-                let message: Message = message.unwrap();
+    let (mut sender, mut receiver) = client.split();
 
-                match message.opcode {
-                    Type::Close => {
-                        let message = Message::close();
-                        sender.send_message(&message).unwrap();
-                        println!("Client {} disconnected", ip);
-                        return;
-                    },
-                    Type::Ping => {
-                        let message = Message::pong(message.payload);
-                        sender.send_message(&message).unwrap();
-                        println!("Client {} pinged", ip);
-                    },
-                    Type::Text => {
-                        let content = from_utf8(&*message.payload).unwrap();
-                        println!("Client {} sent a text message: {}", ip, content);
-                        if content == r#"{"type":"init"}"# {
-                            let cafe_corner = format!(r#"{{"buildingId":0, "name":"Cafe Corner", "lights":[{{"lightId":0, "isOn":false}}, {{"lightId":1, "isOn":false}}]}}"#);
-                            let green_grocer = format!(r#"{{"buildingId":1, "name":"Green Grocer", "lights":[{{"lightId":0, "isOn":false}}, {{"lightId":1, "isOn":false}}]}}"#);
-                            let response = format!(r#"{{"type":"initialize", "buildings":[{}, {}]}}"#, cafe_corner, green_grocer);
-                            println!("Sending response: {:?}", response);
-                            let message = Message::text(response);
-                            sender.send_message(&message).unwrap();
-                        }
-                    },
-                    _ => {
-                        sender.send_message(&message).unwrap();
-                        println!("Client {} sent a message: {:?}", ip, message);
-                    }
+    for message in receiver.incoming_messages() {
+        let message: Message = message.unwrap();
+
+        match message.opcode {
+            Type::Close => {
+                let message = Message::close();
+                sender.send_message(&message).unwrap();
+                println!("Client {} disconnected", ip);
+                return;
+            },
+            Type::Ping => {
+                let message = Message::pong(message.payload);
+                sender.send_message(&message).unwrap();
+                println!("Client {} pinged", ip);
+            },
+            Type::Text => {
+                let content = from_utf8(&*message.payload).unwrap();
+                println!("Client {} sent a text message: {}", ip, content);
+                if content == r#"{"type":"init"}"# {
+                    let cafe_corner = format!(r#"{{"buildingId":0, "name":"Cafe Corner", "lights":[{{"lightId":0, "isOn":false}}, {{"lightId":1, "isOn":false}}]}}"#);
+                    let green_grocer = format!(r#"{{"buildingId":1, "name":"Green Grocer", "lights":[{{"lightId":0, "isOn":false}}, {{"lightId":1, "isOn":false}}]}}"#);
+                    let response = format!(r#"{{"type":"initialize", "buildings":[{}, {}]}}"#, cafe_corner, green_grocer);
+                    println!("Sending response: {:?}", response);
+                    let message = Message::text(response);
+                    sender.send_message(&message).unwrap();
                 }
+            },
+            _ => {
+                sender.send_message(&message).unwrap();
+                println!("Client {} sent a message: {:?}", ip, message);
             }
-        });
+        }
     }
 }
