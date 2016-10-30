26e62a4d15cc68783c0620ed76789d2b7ad41ab5
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2016-10-30 00:05:20 -0400

    - Add town model - Parse town model from json config - Create UDP socket for Arduino - Add town state -> JSON - Respond to client init request by serializing current town state.
    TODO: split into multiple files.

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,27 +5,87 @@ extern crate ws;
 use json::{JsonValue};
 use std::fs::File;
 use std::io::Read;
+use std::net::UdpSocket;
 use ws::{listen, Handler, Handshake, Message, Sender};
+
+struct Town {
+    buildings: Vec<Building>,
+    socket: UdpSocket
+}
+
+struct Building {
+    name: String,
+    lights: Vec<Light>
+}
+
+struct Light {
+    color: [u8; 3]
+}
+
+impl Light {
+    // TODO: Replace this with actual colour.
+    fn color_as_hex_string(&self) -> String {
+        "#ffffff".to_string()
+    }
+}
+
+impl Town  {
+    fn new(socket: UdpSocket, init_data: JsonValue) -> Result<Town, String> {
+        let buildings = match Town::init_buildings(init_data) {
+            Ok(buildings) => buildings,
+            Err(e) => return Err(e)
+        };
+        Ok(Town{
+            buildings: buildings,
+            socket: socket
+        })
+    }
+
+    fn init_buildings(init_data: JsonValue) -> Result<Vec<Building>, String> {
+        Ok(
+            init_data["buildings"].members().map(|b| Building{
+                name: b["name"].to_string(),
+                lights: b["lights"].members().map(|l| Light{
+                    color: [0; 3]
+                }).collect::<Vec<_>>()
+            }).collect::<Vec<_>>()
+        )
+    }
+
+    fn get_state(&self) -> JsonValue {
+        object!{
+            "type" => "initialize",
+            "buildings" => self.buildings.iter().enumerate().map(|(i, b)| object!{
+                "buildingId" => i,
+                "name" => b.name.to_string(),
+                "lights" => b.lights.iter().enumerate().map(|(i, l)| object!{
+                    "lightId" => i,
+                    "color" => l.color_as_hex_string()
+                }).collect::<Vec<_>>()
+            }).collect::<Vec<_>>()
+         }
+    }
+}
 
 struct Client<'a> {
     out: Sender,
-    init_data: &'a JsonValue
+    town: &'a Town
 }
 
 impl<'a> Client<'a> {
-    fn new(out: Sender, init_data: &'a JsonValue) -> Client {
+    fn new(out: Sender, town: &'a Town) -> Client {
         Client{
             out: out,
-            init_data: init_data
+            town: town
         }
     }
 
-    fn handle_init(&mut self) -> Result<(), ws::Error> {
-        let s = format!("{}", self.init_data);
+    fn handle_init(&self) -> Result<(), ws::Error> {
+        let s = format!("{}", self.town.get_state());
         self.out.send(s)
     }
 
-    fn handle_set_building(&mut self) -> Result<(), ws::Error> {
+    fn handle_set_building(&self) -> Result<(), ws::Error> {
         Ok(())
     }
 }
@@ -71,8 +131,8 @@ impl<'a> Handler for Client<'a> {
     }
 }
 
-fn main() {
-    let mut f = match File::open("init-data.json") {
+fn load_init_data(filename: &str) -> JsonValue {
+    let mut f = match File::open(filename) {
         Err(why) => panic!("Couldn't open file: {}", why),
         Ok(file) => file
     };
@@ -82,6 +142,20 @@ fn main() {
         Ok(x) => x
     };
     let init_data = json::parse(s.as_ref()).unwrap();
+    init_data
+}
 
-    listen("0.0.0.0:1234", |out| Client::new(out, &init_data)).unwrap();
+fn main() {
+    let init_data = load_init_data("init-data.json");
+
+    let arduino_addr = "127.0.0.1:12345";
+    let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+    let msg = "hello";
+    let buf = msg.as_bytes();
+    socket.send_to(buf, arduino_addr).unwrap();
+    println!("Sent msg: {}", msg);
+
+    let town = Town::new(socket, init_data).unwrap();
+
+    listen("0.0.0.0:1234", |out| Client::new(out, &town)).unwrap();
 }
