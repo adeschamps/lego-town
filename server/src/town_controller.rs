a3ae44241cdc1175f527c8f3e08574f7defb4384
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2016-11-01 01:54:43 -0400

    Change type of channel to client_api::Msg.

--- server/src/town_controller.rs
+++ server/src/town_controller.rs
@@ -1,22 +1,21 @@
+use client_api;
 use town;
 
 use std::net::{SocketAddr, ToSocketAddrs, UdpSocket};
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc;
 
-pub struct TownCommand {}
-
 pub struct TownController {
     arduino_socket: UdpSocket,
     arduino_addr: SocketAddr,
     town: Arc<Mutex<town::Town>>,
-    commands: mpsc::Receiver<TownCommand>
+    commands: mpsc::Receiver<client_api::Msg>
 }
 
 impl TownController {
     pub fn new<A: ToSocketAddrs>(arduino_addr: A,
                                  town: Arc<Mutex<town::Town>>,
-                                 commands: mpsc::Receiver<TownCommand>
+                                 commands: mpsc::Receiver<client_api::Msg>
                                 ) -> TownController {
         let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
         let addr = arduino_addr.to_socket_addrs().unwrap().next().unwrap();
@@ -30,7 +29,19 @@ impl TownController {
 
     pub fn run(&self) {
         println!("Running town controller.");
-        let cmd = self.commands.recv().unwrap();
-        println!("Received command (but not handled)")
+        for cmd in self.commands.iter() {
+            println!("Received command");
+
+            match cmd {
+                client_api::Msg::Init => {}
+
+                client_api::Msg::SetLight{..} => {
+                    println!("Set light")
+                },
+                client_api::Msg::SetBuilding{..} => {
+                    println!("Set Building")
+                }
+            };
+        }
     }
 }
