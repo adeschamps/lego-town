02c530c1c00d0968fd6f1f3d42671596cadfedd6
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2016-11-12 11:01:58 -0500

    Optionally parse the port number from command line.

--- mock-arduino/main.cpp
+++ mock-arduino/main.cpp
@@ -1,9 +1,22 @@
 #include "light_controller.hpp"
 
+#include <sstream>
+
 // Drive the main logic of the Arduino.
-int main()
+int main(int argc, char* argv[])
 {
-  Poco::Net::SocketAddress address (Poco::Net::IPAddress(), 12345);
+  // Optionally parse the port from the command line
+  auto port = [&]{
+    uint16_t result = 12345;
+    if (argc > 1) {
+      auto ss = std::stringstream(argv[1]);
+      uint16_t port;
+      if (ss >> port) { result = port; }
+    }
+    return result;
+  }();
+
+  Poco::Net::SocketAddress address (port);
   LightController arduino (address);
   arduino.run();
 }
