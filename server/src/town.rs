51bd57bcca9003293a1d623eafdbe7f73684332d
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2016-12-06 23:31:06 -0500

    Add id fields to lights and buildings.

--- server/src/town.rs
+++ server/src/town.rs
@@ -10,24 +10,25 @@ pub struct Town {
 
 #[derive(Clone, Debug, PartialEq, RustcDecodable, RustcEncodable)]
 pub struct Building {
+    pub id: u8,
     pub name: String,
     pub lights: Vec<Rc<Light>>
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Light {
+    pub id: u8,
     pub color: messages::Color
 }
 
 impl Decodable for Light {
     fn decode<D: Decoder>(d: &mut D) -> Result<Self, D::Error> {
-        d.read_struct("Light", 0, |d| {
-            d.read_struct_field("color", 0, messages::Color::decode)
-        }).map(|color| {
-            Light{
-                color: color
-            }
-        })
+        d.read_struct("Light", 0, |d| Ok(Light{
+            id:
+                d.read_struct_field("id", 0, D::read_u8)?,
+            color:
+                d.read_struct_field("color", 1, messages::Color::decode)?
+        }))
     }
 }
 
@@ -38,30 +39,26 @@ impl Encodable for Light {
 }
 
 #[cfg(test)]
-mod tests {
-    use messages;
-    use super::*;
-    use rustc_serialize::json;
-    use std::rc::Rc;
+use rustc_serialize::json;
 
-    #[test]
-    fn decode_town() {
-        let light = r##"{"color":"RED"}"##;
-        let cafe_corner = format!("{{\"name\":\"Cafe Corner\", \"lights\": [{}, {}]}}", light, light);
-        let init_data = format!("{{\"buildings\":[{}]}}", cafe_corner);
+#[test]
+fn decode_town() {
+    let lights = r##"[{"id":0, "color":"RED"}, {"id":1, "color":"RED"}]"##;
+    let cafe_corner = format!("{{\"id\":0, \"name\":\"Cafe Corner\", \"lights\": {}}}", lights);
+    let init_data = format!("{{\"buildings\":[{}]}}", cafe_corner);
 
-        let town : Town = json::decode(init_data.as_str()).unwrap();
-        let expected = Town {
-            buildings: vec![
-                Rc::new(Building {
-                    name: "Cafe Corner".to_string(),
-                    lights: vec![
-                        Rc::new(Light{ color: messages::Color::RED }),
-                        Rc::new(Light{ color: messages::Color::RED })
-                    ]
-                })
-            ]
-        };
-        assert_eq!(town, expected);
-    }
+    let town : Town = json::decode(init_data.as_str()).unwrap();
+    let expected = Town {
+        buildings: vec![
+            Rc::new(Building {
+                id: 0,
+                name: "Cafe Corner".to_string(),
+                lights: vec![
+                    Rc::new(Light{ id: 0, color: messages::Color::RED }),
+                    Rc::new(Light{ id: 1, color: messages::Color::RED })
+                ]
+            })
+        ]
+    };
+    assert_eq!(town, expected);
 }
